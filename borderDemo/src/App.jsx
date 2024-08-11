574d9603a973b7e252d71c5027714feab3d44c77
Hartslief <calebhartslief@gmail.com>
2024-08-11 15:06:15 +0200

    first commit, but already done

---
+++ borderDemo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='card'>
+        <h1>Hello World!</h1>
+        <p>Welcome to this test text block, 
+          this is just meant to be filled 
+          with nonsensical words to take 
+          up some space, actually quite a bit of space.
+        </p>
+    </div>
+  )
+}
+
+export default App
